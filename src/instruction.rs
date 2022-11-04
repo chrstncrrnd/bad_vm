6e5380ef743b7d81931a0f666c350fd716dde84c
ChrstnC <chrstncrrnd@gmail.com>
2022-11-04 16:45:15 +0100

    initial commit

---
+++ src/instruction.rs
@@ -0,0 +1,32 @@
+
+#[derive(Debug, PartialEq)]
+pub enum Opcode {
+    HLT,
+    IGL
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Instruction {
+    opcode: Opcode,
+
+}
+
+impl Instruction {
+    pub fn new(opcode: Opcode) -> Self{
+        Self{
+            opcode
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_create_instruction() {
+        let instruction = Instruction::new(Opcode::HLT);
+        assert_eq!(instruction.opcode, Opcode::HLT);
+    }
+}
